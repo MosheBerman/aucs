31b97aba5edb16cea597d65dc26b006fb7764877
Moshe Berman <moshefromsinai@gmail.com>
2015-02-11 14:11:29 -0500

    Added local configuration for testing.

--- application/config/database.php
+++ application/config/database.php
@@ -33,36 +33,47 @@
 | 				 can make your site vulnerable to SQL injection if you are using a
 | 				 multi-byte character set and are running versions lower than these.
 | 				 Sites using Latin-1 or UTF-8 database character set and collation are unaffected.
-|	['swap_pre'] A default table prefix that should be swapped with the dbprefix
+|	['swap_pre'] A local_test table prefix that should be swapped with the dbprefix
 |	['autoinit'] Whether or not to automatically initialize the database.
 |	['stricton'] TRUE/FALSE - forces 'Strict Mode' connections
 |							- good for ensuring strict SQL while developing
 |
 | The $active_group variable lets you choose which connection group to
-| make active.  By default there is only one group (the 'default' group).
+| make active.  By local_test there is only one group (the 'local_test' group).
 |
 | The $active_record variables lets you determine whether or not to load
-| the active record class
+| the active record /**
+* 
+*/
+/**
+class ClassName extends AnotherClass
+{
+	
+	function __construct(argument)
+	{
+		# code...
+	}
+}
 */
 
-$active_group = 'default';
+$active_group = 'local_test';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
-$db['default']['db_debug'] = TRUE;
-$db['default']['cache_on'] = FALSE;
-$db['default']['cachedir'] = '';
-$db['default']['char_set'] = 'utf8';
-$db['default']['dbcollat'] = 'utf8_general_ci';
-$db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
+$db['local_test']['hostname'] = 'localhost';
+$db['local_test']['username'] = 'aucs';
+$db['local_test']['password'] = '';
+$db['local_test']['database'] = 'aucs';
+$db['local_test']['dbdriver'] = 'mysql';
+$db['local_test']['dbprefix'] = '';
+$db['local_test']['pconnect'] = TRUE;
+$db['local_test']['db_debug'] = TRUE;
+$db['local_test']['cache_on'] = FALSE;
+$db['local_test']['cachedir'] = '';
+$db['local_test']['char_set'] = 'utf8';
+$db['local_test']['dbcollat'] = 'utf8_general_ci';
+$db['local_test']['swap_pre'] = '';
+$db['local_test']['autoinit'] = TRUE;
+$db['local_test']['stricton'] = FALSE;
 
 
 /* End of file database.php */
