b905db1c35a2eae8268f1fe5b2c0c1274eabd5c1
Moshe Berman <moshefromsinai@gmail.com>
2015-02-15 19:06:38 -0500

    Tweaked prize.php to add prizes.

--- application/models/prize.php
+++ application/models/prize.php
@@ -3,14 +3,27 @@
 class Prize extends CI_Model {
 
     public $prize_id = NULL;
-    public $prize_name = '';
-    public $prize_description = '';
-    public $sponsor_id = '';
-    public $package_id = '';
+    public $prize_name = NULL;
+    public $prize_description = NULL;
+    public $sponsor_name = NULL;
+    public $package_id = NULL;
 
     function __construct()
     {
         parent::__construct();
+    }
+
+    public function create_prize() {
+
+        $data = array(
+            'prize_name' => $this->prize_name,
+            'prize_description' => $this->prize_description,
+            'sponsor_name' => $this->sponsor_name,
+            'package_id' =>
+        );
+
+        $this->db->insert('prize', $data);
+
     }
 
 }
